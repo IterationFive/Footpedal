7672dfda71b109ec8b89038524b5bee22ac7b1dd
Cather Steincamp <catalyst@catalystpoint.org>
2023-01-17 23:02:56 -0500

    Basic config menus

---
+++ FootPedal/FootPedal/menus/HandbrakeConfig.py
@@ -0,0 +1,27 @@
+'''
+Created on Jan 16, 2023
+
+@author: Cather Steincamp
+'''
+
+import CursedUtils as cu
+
+class HandbrakeConfig(cu.ConfigMenu):
+    '''
+    classdocs
+    '''
+
+
+    def setup(self):
+        self.addField('Handbrake CLI', 75, 'The location of the HandBrakeCLI executable', self.isFile )
+        self.addField('Preset', 40, '(Optional) One of the built-in HandPresets' )
+        self.addField('GUI Preset', 40, '(Optional) A Preset Created and named using the GUI' )
+        self.addField('JSON Config', 75, '(Optional) A JSON file containing Handbrake configuration', self.isFileOrNone )
+        self.addField('Subtitle Language', 40, 'The default language code for subtitle files' )
+        
+    def isFileOrNone(self, validate):
+        
+        if validate == '': 
+            return True
+        else:
+            return self.isFile( validate )
