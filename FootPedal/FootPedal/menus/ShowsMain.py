9d8e6ec94539736f84f742ff5d4a672e1fede6c4
Cather Steincamp <catalyst@catalystpoint.org>
2023-01-19 21:08:11 -0500

    added blank functions, preliminary list builder & key assignments

--- FootPedal/FootPedal/menus/ShowsMain.py
+++ FootPedal/FootPedal/menus/ShowsMain.py
@@ -8,6 +8,7 @@ from FootPedal import SHOWCONFIG
 import os, json
 
 import CursedUtils as cu
+from pkg_resources._vendor.more_itertools.more import padded
 
 class ShowMenu(cu.Window):
     '''
@@ -16,6 +17,7 @@ class ShowMenu(cu.Window):
     def __init__(self, parent:cu.Screen):
         self.configFile = SHOWCONFIG
         self.loadConfig()
+        self.keys = cu.KeyResponder()
         cu.Window.__init__(self, parent, 25, 110, 1, 5)
     
     def loadConfig(self):
@@ -81,14 +83,110 @@ class ShowMenu(cu.Window):
                 
         self.setSlot( 'blurb1', 21, midpoint - 25, 50, cu.CENTER )  
         self.setSlot( 'blurb2', 22, midpoint - 25, 50, cu.CENTER )  
-        self.setSlot( 'blurb3', 23, midpoint - 25, 50, cu.CENTER )        
-                
-        self.slotWrite( 'blurb2', 'Press C to change TVDB Configuration' )
-        self.slotWrite( 'blurb3', 'Press Escape to Exit' )
-                
+        self.setSlot( 'blurb3', 23, midpoint - 25, 50, cu.CENTER )  
+        self.setSlot( 'blurb4', 24, midpoint - 25, 50, cu.CENTER )
+           
+        self.slotWrite( 'blurb2', 'Press A to add a show')        
+        self.slotWrite( 'blurb3', 'Press C to change TVDB Configuration' )
+        self.slotWrite( 'blurb4', 'Press Escape to Exit' )
         
-                
+        self.listStart = 0
         
+        self.setupListMenu()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    def setupListMenu(self):
+        
+        self.keys.clearAll()
+        # this gets rebuilt every time.
+        
+        if 'shows' not in self.config:
+            self.config['shows'] = {}
+        else:
+            shows = []
+            for show in self.config['shows']:
+                shows.append( show )
+                
+            # this also gets rebuilt every time.  
+                
+            totalShows = len( shows )
+                
+            if totalShows > 0:
+                #great!  we have work to do
+                
+                pageEnd = self.listStart + 9
+                
+                if pageEnd >= totalShows:
+                    pageEnd = totalShows - 1
+                    
+            if totalShows > 10:
+                self.window.slotWrite( 'pagecount', str( self.listStart + 1 ) + '-' + str( pageEnd + 1 ) + ' of ' + str( totalShows ))
+                
+            if self.listStart > 1:
+                self.window.slotWrite( 'navUp', 'Up/PageUp')
+                self.keys.setResponse( 'pageup', self.listPageUp )
+                self.keys.setResponse( 'up', self.listUp )
+                
+            if self.pageEnd < ( totalShows - 1 ):
+                self.windowSlotWrite( 'navDown', 'Down/PageDown')
+                self.keys.setResponse( 'pagedown', self.listPageDown )
+                self.keys.setResponse( 'down', self.listDown )                
+                
+            i = 0
+            
+            while i < 10 :
+                if i == 9:
+                    key = "0"
+                else:
+                    key = str( i+1 )  
+                    
+                showname = shows[ self.listStart + i ]
+                
+                self.writeSlot( 'key' + key, key )
+                self.writeSlot( 'name' + key, showname )
+                
+                self.keys.setResponse( key, self.editShow, args=[showname] )
+                
+                i += 1
+                
+                if self.listStart + i > pageEnd:
+                    break
+                
+                    
+            
+        # setup other menu keys
+        self.keys.setResponse( 'A', self.addShow )
+        self.keys.setResponse( 'C', self.tvdbConfig )
+        
+        
+        
+    def editShow(self, showname):
+        pass
+        
+    def listUp(self):
+        pass
+    
+    def listDown(self):
+        pass
+    
+    def listPageUp(self):
+        pass
+    
+    def listPageDown(self):
+        pass
+    
+    def addShow(self):
+        pass
+        
+    def tvdbConfig(self):
+        pass
         
         
         
