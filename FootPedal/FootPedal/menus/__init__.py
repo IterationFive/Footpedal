b0b94098b5402550f0c2f4b24f6a98666b007494
Cather Steincamp <catalyst@catalystpoint.org>
2023-01-20 05:48:58 -0500

    updated imports

--- FootPedal/FootPedal/menus/__init__.py
+++ FootPedal/FootPedal/menus/__init__.py
@@ -1,3 +1,4 @@
 from FootPedal.menus.HandbrakeConfig import HandbrakeConfig
 from FootPedal.menus.PathConfig import PathConfig
 from FootPedal.menus.MainMenu import MainMenu
+from FootPedal.menus.ShowsMain import ShowMenu
