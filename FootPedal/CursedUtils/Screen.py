fec3c3a7afead614c059348c6d659823e16cb73b
Cather Steincamp <catalyst@catalystpoint.org>
2023-01-22 04:44:50 -0500

    documentation and functionality updates, see full message
    - removed extraneous assignment of .cursorState
    - corrected convulted reference to same
    - added .writeCentered()
    - added .slotGet()
    - added forceAlign option to .slotWrite() and functionality
    to support it
    
    documentation was both incomplete and out of date

--- FootPedal/CursedUtils/Screen.py
+++ FootPedal/CursedUtils/Screen.py
@@ -25,15 +25,119 @@ class Screen(object):
         echo
             determines whether keystrokes appear on the screen.  
             defaults to False, because man, that's a recipe for disaster.
-            This setting can be changed with echo()
+            This setting can be changed with setEcho()
+        cursor
+            determines wheter the cursor blinks on screen, and keeps track
+            of that state in .cursorState
     
     The following "methods" are really just pointers to the screen object's methods:
     
-        nodelay
-        getch
-        refresh
-        write (points to window.addstr)
+        .nodelay
+        .getch
+        .refresh
+        .write (points to window.addstr)
+        
+    The following methods control the behavior of the window itself.
     
+        .setSize( y,x )
+            sets the size of the window, and keeps track of the values
+            for access in sizeY and sizeX
+            
+        .setEcho( bool )
+            disables or enables keyboard echo on the screen.
+            tracks state in .echo
+            
+        .setCursor( bool )
+            disables or re-enables the blinking cursor. 
+            tracks state in .cursorState
+            
+        .close()
+            ends the curses session
+            
+        .reopen()
+            restores the curses session.
+        
+    The following methods are provided to assist with text placement.
+    
+        .writeNow()
+            .write(), but with automatic refresh
+        .writeCentered( y, text, refresh, midpoint )
+            puts the text on line y, centered.
+            if midpoint is provided and not None,
+            the text will be centered on that point.
+            if refresh is True, screen will refresh immediately.
+            
+    Additionally, one can create 'slots', which are preset locations
+    with a specified length and alignment.  
+    
+        .setSlot(name, y, x, width)
+            name
+                string, if not unique will override pre-existing values.
+            y
+                the line where the slot exists.
+            x
+                the leftmost point of the slot
+            width
+                the maximum number of characters the slot will display
+            alignment
+                uses the special variables from CursedUtils.  default is cu.LEFT
+                
+        .slotWrite(name,text,refresh,forceAlign)
+            name
+                the predefined slot name.  If the slot does not exist, this 
+                method will do nothing.
+            text
+                the text to be displayed in the slot
+            refresh
+                defaults to False
+                if True, screen will refresh after writing the text.
+            forceAlign
+                defaults to False
+                a special variable from CursedUtils; overrides the predefined
+                alignment this time.  (does not change the defined alignment)
+                
+            Displays the provided text in the slot and stores it for retrieval
+            using .slotGet()            
+            
+            If the length of the text exceeds the width of the slot, the text
+            will be truncated, although the full value will be stored.
+            
+        .slotBlank(name, refresh)
+            name
+                the predefined slot name.  If the slot does not exist, this
+                method will do nothing.
+            refresh
+                defaults to False
+                if True te screen will refresh automatically.
+                
+        .slotGet(name)
+            returns the value stored in the slot named.
+            if slot does not exist, will return None.
+            
+    This class also provides two methods for inputing a block of text
+    uses curses' Textbox functionality.
+    
+        .lineInput( y, x, length, prefill)
+            y,x
+                the starting position of the textbox
+            length
+                the total number of characters of the textbox
+            prefill
+                defaults to blank
+                if provided, will be the default value of the textbox
+                
+            puts a text input box at the screen at the specified 
+            coordinates and width.  returns the text entered by the user,
+            stripped of leading/trailing spaces.
+            
+            Once text is entered, the textbox and its contents will
+            disappear.
+            
+        .slotInput( slot )
+            uses the contents and location of the slot for the textbox.
+            
+            once text is entered, the slot will be updated with the 
+            provided text and the screen will update automatically.
     
     '''
 
@@ -42,7 +146,6 @@ class Screen(object):
         Constructor
         '''
         
-        self.cursorState = True
         self.offsetY = 0
         self.offsetX = 0
         self.slot = {}
@@ -91,11 +194,11 @@ class Screen(object):
     def setCursor(self, state=True):
         if state == True:
             curses.curs_set(1)
-            self.screen.cursorState = True
+            self.cursorState = True
             
         else: 
             curses.curs_set(0)
-            self.screen.cursorState = False
+            self.cursorState = False
         
     def close(self):
         curses.endwin()
@@ -109,6 +212,14 @@ class Screen(object):
     def writeNow(self, *args, **kwargs):
         self.window.addstr( *args, **kwargs )
         self.refresh()
+        
+    def writeCentered(self, y, text:str, refresh=False, midpoint=None):
+        if midpoint is None:
+            self.write( y, int( ( self.sizeX - len(text) ) / 2 ), text )
+        else:
+            self.write( y, midpoint - int( len(text) / 2  ), text )
+        if refresh:
+            self.window.refresh()
     
     def lineInput(self, y, x, length, prefill='' ):
         '''
@@ -137,14 +248,21 @@ class Screen(object):
         return r 
     
     def setSlot(self, name, y, x, width, alignment=cu.LEFT):
-        self.slot[name] = { 'y':y,'x':x,'w':width,'a':alignment,'c':'' }    
+        self.slot[name] = { 'y':y,'x':x,'w':width,'a':alignment,'c':'' }
+        
+    def slotGet(self, slot):
+        if slot not in self.slot:
+            return None
+        else:
+            return self.slot[slot]['c'] 
         
     def slotBlank(self, slot, refresh=False ):
         self.window.addstr( self.slot[slot]['y'], self.slot[slot]['x'], ' ' * self.slot[slot]['w'] )
         if refresh:
             self.window.refresh()
         
-    def slotWrite(self, slot, text, refresh=False):
+    def slotWrite(self, slot, text, refresh=False, forceAlign=False):
+        
         if slot in self.slot:
             self.slotBlank(slot)
             slot = self.slot[slot]
@@ -154,11 +272,17 @@ class Screen(object):
                 text = text[:slot['w']]                
             
             x = slot['x']
+            
+            if forceAlign != False:
+                a = forceAlign
+            else:
+                a = slot['a']
+            
             blanks = slot['w'] - len( text )
             
-            if slot['a'] == cu.RIGHT:
+            if a == cu.RIGHT:
                 x += blanks
-            if slot['a'] == cu.CENTER and blanks > 0:
+            if a == cu.CENTER and blanks > 0:
                 x += int( blanks / 2 )
                 
             self.write( slot['y'], x, text )
